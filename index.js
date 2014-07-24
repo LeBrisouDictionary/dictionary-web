8366aa49d1f9fc0ea16db20a25c4f5f580c1a9df
amaurybrisou <amaury.brisou@gmail.com>
2014-07-24 16:15:21 +0200

    new tests

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-exports.register = function (plugin, options, next) {	
-	process.env.dictionary_web_base_dir = __dirname
+exports.register = function (plugin, options, next) {
+  process.env.dictionary_web_base_dir = __dirname
 
-	plugin.route(require('./routes'))
+  plugin.route(require('./routes'))
 
-	var selection = plugin.select('dictionary-web').servers[0]
+  var selection = plugin.select('dictionary-web').servers[0]
 
   next()
 }
 
 exports.register.attributes = {
-	name: 'dictionary-web',
+  name: 'dictionary-web',
   version: '0.0.1',
   // pkg: require('./package.json')
 }
--- public/js/directives/view-node.js
+++ public/js/directives/view-node.js
@@ -1,104 +1,116 @@
-
-
 angular.module('LeBrisouBackend.directives', [])
-    .directive('viewNode', [ '$compile', function($compile){
+  .directive('viewNode', ['$compile',
+    function ($compile) {
 
-        var internals = {};
+      var internals = {};
 
-        // internals.table = function(e){
-        //     var table = d3.select(e)
-        //         .append('table')
-        //         .attr('class', 'cl-lg-6 table table-responsive table-bordered table-hover top20'),
-        //         thead = table.append('thead').attr('ng-hide', '$index'),
-        //         tbody = table.append('tbody');
-        
-        //     thead.append('tr')
-        //     .selectAll('th')
-        //     .data(['#', 'Id', 'Lema', 'Pos', 'gerund', 'participle', 'Language', 'Delete' ])
-        //     .enter()
-        //     .append('th')
-        //     .attr('class', 'entry')
-        //     .text(function(entry){ return entry; });
+      // internals.table = function(e){
+      //     var table = d3.select(e)
+      //         .append('table')
+      //         .attr('class', 'cl-lg-6 table table-responsive table-bordered table-hover top20'),
+      //         thead = table.append('thead').attr('ng-hide', '$index'),
+      //         tbody = table.append('tbody');
 
-        //     return tbody;
-        // }
+      //     thead.append('tr')
+      //     .selectAll('th')
+      //     .data(['#', 'Id', 'Lema', 'Pos', 'gerund', 'participle', 'Language', 'Delete' ])
+      //     .enter()
+      //     .append('th')
+      //     .attr('class', 'entry')
+      //     .text(function(entry){ return entry; });
 
-        internals.addRow = function (attach) {
-        
-            attach.each(function(entry){
-                var colnames,
-                    key,
-                    value,
-                    table = d3.select(this);
+      //     return tbody;
+      // }
+      internals.word = ['id', 'lema', 'gerund', 'participle', 'language', 'register']
+      internals.addRow = function (attach) {
 
-                if(entry instanceof Object){
-                    entry = [entry]
-                }
+        attach.each(function (entry) {
+          var colnames,
+            key,
+            value,
+            table = d3.select(this);
 
-                colnames = entry                                             // array of objects
-                .reduce(function(p,c) { return p.concat(d3.keys(c)); }, [])  // array with all keynames
-                .reduce(function(p,c) { return (p.set(c,0), p); }, d3.map()) // map with unique keynames as keys
-                .keys();
+          if (entry instanceof Object) {
+            entry = [entry]
+          }
 
+          colnames = entry // array of objects
+          .reduce(function (p, c) {
+            return p.concat(d3.keys(c));
+          }, []) // array with all keynames
+          .reduce(function (p, c) {
+            return (p.set(c, 0), p);
+          }, d3.map()) // map with unique keynames as keys
+          .keys();
 
-                // TITLES
-                console.log(colnames)
-                key = table.append("thead").selectAll("th")
-                .data(colnames)
-                .enter().append("th").attr('class', 'key');
+          var f_colnames = colnames.filter(function (d) {
+            if (internals.word.indexOf(d) != -1) return d;
+          })
 
-                key.filter(function(d) {
-                    return (!(d instanceof Number));
-                })
-                .text(function(d) { return d; });
-
-
-                // BODY
-                value = table.append('tbody').selectAll('th')
-                .data(function(e){
-                    return colnames.map(function(k){
-                        return e[k] || "";
-                    });
-                })
-                .enter().append('th').attr('class', 'value' );
-
-                // Object.keys(d)[0][
-
-                // value.filter(function(d){
-                //     return (d instanceof Object) ? Object.keys(d)[0] : false
-                // });
-
-                value.filter(function(d) {
-                    return (!(d instanceof Array) && !(d instanceof Object));
-                })
-                .text(function(d) { return d; });
-                
-                value.filter(function(d) {
-                    return ((d instanceof Array) || (d instanceof Object)); 
-                })
-                .append("table")
-                .attr('class', 'subtable')
-                .call(internals.addRow);
+          // TITLES
+          console.log(colnames)
+          key = table.append("div")
+            .attr('class', 't')
+            .selectAll("div")
+            .data(f_colnames)
+            .enter().append("div").attr('class', 'key')
+            .text(function (e) {
+              return e;
             });
+
+          // BODY
+          value = table.append('div').attr('class', 't').selectAll('div')
+            .data(function (e) {
+              return f_colnames.map(function (k) {
+                if (e[k] instanceOf Object) {
+                  return
+                }
+                console.log('ZZZZZZZ', k, e);
+                return e[k] || "";
+              });
+            })
+            .enter().append('div').attr('class', 'key')
+
+
+          // Object.keys(d)[0][
+
+          // value.filter(function(d){
+          //     return (d instanceof Object) ? Object.keys(d)[0] : false
+          // });
+
+          value.filter(function (d) {
+            return (!(d instanceof Array) && !(d instanceof Object));
+          })
+            .text(function (d) {
+              return d;
+            });
+
+          value.filter(function (d) {
+            return ((d instanceof Array) || (d instanceof Object));
+          })
+            .append("table")
+            .attr('class', 'subtable')
+            .call(internals.addRow);
+        });
+      }
+
+
+      return {
+        restrict: 'A',
+        link: function (scope, element, attrs) {
+          console.log(scope.entries);
+
+          d3.select(element[0])
+            .append('div').selectAll('div')
+            .data(scope.entries)
+            .enter().append('div').attr(
+              'class',
+              'word')
+            .call(internals.addRow);
+
+          element.removeAttr("view-node");
+          $compile(element)(scope);
         }
-
-
-        return {
-            restrict: 'A',
-            link: function(scope, element, attrs){
-                console.log(scope.entries);
-
-                d3.select(element[0])
-                .append('table').selectAll('table')
-                .data(scope.entries)
-                .enter().append('tr').attr(
-                    'class',
-                    'word')
-                .call(internals.addRow);
-            
-                element.removeAttr("view-node");
-                $compile(element)(scope);
-            }
-        }
+      }
     }
-]);
+  ]);
