21202a4f11798d42d88b00e171f4ed70d7fef0b8
donGog <thomasgoguelin@gmail.com>
2014-07-31 18:31:02 +0200

    grid style - basis

--- public/js/controllers/add.js
+++ public/js/controllers/add.js
@@ -1,23 +1,23 @@
 angular.module('LeBrisouBackend.controllers', ['LeBrisouBackend.config'])
   .controller('MainCtrl', [ '$scope', '$http', 'apiUrl', function($scope, $http, apiUrl) {
-  	$scope.def_ex = {};
-  	$scope.insert = function($attrs) {
+  	
+    $scope.def_ex = {};
+  	
+    $scope.insert = function($attrs) {
   		$scope.def_ex['lema'] = $attrs.lema;
   		$scope.def_ex['pos'] = $attrs.pos;
   		console.log($scope.def_ex);
   	}
 
   	$scope.query = function(){
-
       var url = apiUrl + '/words';
-      
   		$http.get(url).
       success(function(data) {
         if(data && data.result){
-          console.log(data.result)
+          console.log(data.result);
           $scope.entries = data.result;
-
         }
       });
   	}
+
 }]);
