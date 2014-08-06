345dbeed1990a1e7dca8ad6ffda8da28b698a68d
Amaury Brisou <amaury.brisou@gmail.com>
2014-08-06 13:22:30 +0000

    add.html

--- public/js/directives/index.js
+++ public/js/directives/index.js
@@ -1,17 +1,28 @@
 angular.module('LeBrisouBackend.directives', [])
+	.directive('definitionExample', ['$compile', function($compile){
+		return {
+			restrict: 'E',
+			templateUrl: '/directives/definition_example.html',
+			controller: function($scope, $element ){
+				$scope.addElement = function($scope, $element){
+					var new_element = $element.clone();
+					var el =  $compile(new_element)( $scope );				
+					$element.append( el );	
+				};
+			}
+		};
+	}])
 	.directive('addDefinition', ['$compile', function($compile){
 			var definition_id = 1;
 			var definition_example_id = 1;
-			return {			
+			return {
+				require: '^definitionExample',
 				restrict: 'A',	
 				templateUrl: '/directives/definition.html',
-				require: '?ngModel',
 				scope: true,
-				controller: function ($scope, $element) {
+				controller: function($scope, $element){
 					$scope.addDefinition = function(){
-						var new_element = $element.clone();
-						var el =  $compile(new_element)( $scope );				
-  					$element.parent().append( el );	
+						$scope.addElement($scope, $element);
 					};
 				},
 				link: function($scope, $element, attrs, controller){
@@ -37,24 +48,24 @@ angular.module('LeBrisouBackend.directives', [])
 	}])
 	.directive('addExample', ['$compile', function($compile){
 			var example_id = 1;
-			return {			
+			var that =  {
+				require: '^definitionExample',
 				restrict: 'A',	
 				templateUrl: '/directives/example.html',
-				require: '?ngModel',
-				scope: true,
-				controller: function ($scope, $element) {
+				controller: function($scope, $element){
 					$scope.addExample = function(){
-  					var new_element = $element.clone();
-  					var el =  $compile(new_element)( $scope );				
-    				$element.parent().append( el );	
+						$scope.addElement($scope, $element);
 					};
 				},
+				scope: true,
 				link: function($scope, $element, attrs, controller){
 					
 					$scope.example_id = example_id++;
 					
 					
 				}
+				
 			};
+			return that;
 	}]);
 
